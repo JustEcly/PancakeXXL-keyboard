cf3e651e3d14ac542d9a9ef6da654d059b8040cc
JustEcly <aymeric.mathieu@outlook.com>
2025-04-28 23:23:45 +0200

    QMK-Vial make
    Il finish to make the Vial and QMK code

---
+++ QMK/PancakeXXL/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDD, 0x10, 0x01, 0x3A, 0x18, 0x58, 0x2A, 0x0D}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
