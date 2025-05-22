92107c7e376a6d151f6292eeb0ea0f5852c7c43d
JustEcly <aymeric.mathieu@outlook.com>
2025-05-22 13:42:13 +0200

    PCB last modification

--- QMK/PancakeXXL/keymaps/vial/config.h
+++ QMK/PancakeXXL/keymaps/vial/config.h
@@ -3,5 +3,10 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0xDD, 0x10, 0x01, 0x3A, 0x18, 0x58, 0x2A, 0x0D}
+
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define VIAL_TAP_DANCE_ENTRIES 10
+
